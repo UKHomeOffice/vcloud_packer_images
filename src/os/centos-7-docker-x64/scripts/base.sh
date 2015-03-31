d441550e873e4229b7308b3957726e1bc3129119
Allan Degnan <allan@adegnan.net>
2015-03-31 17:42:52 +0100

    added debian

--- src/os/centos-7-docker-x64/scripts/base.sh
+++ src/os/centos-7-docker-x64/scripts/base.sh
@@ -1,3 +1,3 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 rpm -Uvh https://anorien.csc.warwick.ac.uk/mirrors/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl docker cloud-init
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` docker
